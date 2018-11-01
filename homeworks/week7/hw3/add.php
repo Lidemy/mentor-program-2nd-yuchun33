60c758ff43792425fc1ce1bf685108a2ac385f31
yuchun <yuchunlin33@gmail.com>
2018-11-02 01:55:14 +0800

    ajax response

--- homeworks/week7/hw3/add.php
+++ homeworks/week7/hw3/add.php
@@ -3,42 +3,11 @@
 
     if(isset($_COOKIE['cookie'])){
         $username = $_COOKIE['cookie'];
-        echo $username;
         $content = $_POST['content'];
-        echo $content;
         $timestamp = date('m-d H:i:s', strtotime('+8 hours'));  
-        echo  $timestamp; 
         $parent = $_POST['parent'];
-        echo  $parent;    
     }
 
-
-
-/*
-    if($parent == 0){
-        $table = 'yuchun_board';
-    }
-    else{
-        $table = 'yuchun_board' . $parent;
-        echo $table . '<br>';
-    }
-
-    $sql = 'CREATE TABLE IF NOT EXISTS ' . $table . '(
-            ID int NOT NULL AUTO_INCREMENT,
-            user varchar(30),
-            content varchar(255),
-            timestamp varchar(30),
-            parent int,
-            PRIMARY KEY (ID))';
-    echo $sql;
-
-    
-    if($conn->query($sql)===TRUE){
-        //echo 'Table ' . $table . ' created successfully' . '<br>';
-    } else {
-        echo 'error creating table: ' . $conn->error . '<br>';
-    }
-*/
     if(!empty($_COOKIE['vertify'])){
         $table = 'yuchun_board';
         $sql = "INSERT INTO $table (user, content, timestamp, parent) VALUES (?, ?, ?, ?)";
@@ -46,20 +15,16 @@
         $stmt->bind_param("sssi",$username,$content,$timestamp,$parent);
 
         if($stmt->execute()===TRUE){
-            echo 'New record created successfully' . '<br>';
             $last_id = $conn->insert_id;
-            echo $last_id;
-
-            header("lastID: $last_id");
-            header("content: $content");
-            header("parent: $parent");
-
+            $comment = array(
+                "content"=>$_POST['content'],
+                "ID"=>$last_id
+            );
+            echo json_encode($comment);
         } else {
-            echo $sql . '<br>';
-            echo 'error creating record: ' . $conn->error . '<br>';
+            //echo $sql . '<br>';
+            //echo 'error creating record: ' . $conn->error . '<br>';
         }
-        //$url = './memberBoard.php';
-        //header("Location: " . $url);   
     }else{
 ?>
     <script>
