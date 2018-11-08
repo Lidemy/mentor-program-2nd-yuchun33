75bf7ebdfdd612daa93b2313d136f01e5b663ec9
yuchun33 <yuchunlin@gmail.com>
2018-11-08 16:28:47 +0800

    修改 gulpfile

--- homeworks/week10/hw3/gulpfile.js
+++ homeworks/week10/hw3/gulpfile.js
@@ -3,11 +3,14 @@ var compass = require('gulp-compass')
 var minifyCSS = require('gulp-minify-css')
 var uglify = require('gulp-uglify')
 var babel = require('gulp-babel')
-var pump = require('pump')
 var rename = require('gulp-rename')
 var gulpSequence = require('gulp-sequence')
 
+// return 是非同步，才可以確保用的檔案是轉換好的
+
+//轉成 css
 gulp.task('compass', function(){
+
     return gulp.src('./sass/*.sass')
     .pipe(compass({
         css:'./done',
@@ -15,6 +18,7 @@ gulp.task('compass', function(){
     }))
 })
 
+//壓縮 css
 gulp.task('minify-css',function(){
     return gulp.src('./done/*.css')
     .pipe(minifyCSS({
@@ -26,6 +30,7 @@ gulp.task('minify-css',function(){
     .pipe(gulp.dest('./build'))
 })
 
+//轉 ES5
 gulp.task('babel',function(){
     return gulp.src("./main.js")
     .pipe(babel())
@@ -35,17 +40,22 @@ gulp.task('babel',function(){
     .pipe(gulp.dest('./build'))
 })
 
-
-gulp.task('uglify',function(cb){
-    pump([
-        gulp.src('./build/main_ES5.js'),
-        uglify(),
-        rename(function(path){
-            path.basename += '_compass'
-        }),
-        gulp.dest('./build')
-    ],cb)
-
+//壓縮 js
+gulp.task('uglify',function(){
+    return gulp.src('./build/main_ES5.js')
+    .pipe(uglify())
+    .pipe(rename(function(path){
+        path.basename += '_compass'
+    }))
+    .pipe(gulp.dest('./build'))
 })
 
-gulp.task('default',gulpSequence('compass','minify-css','babel','uglify'))
+//監聽：一有變化就可以自動轉換
+gulp.task('watch', function(){
+    watch('./sass/*.sass',['compass'])
+    watch('./done/*.css',['minify-css'])
+    watch('./main.js',['babel'])
+    watch('./build/main_ES5.js',['uglify'])
+})
+
+gulp.task('default',gulpSequence('compass','minify-css','babel','uglify','watch'))
