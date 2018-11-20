9e91037cc78ffc662d5fb9c35d63a56ec6066102
yuchun33 <yuchunlin@gmail.com>
2018-11-20 19:45:41 +0800

    訂正第 11 週作業

--- homeworks/week11/hw2/URLController.js
+++ homeworks/week11/hw2/URLController.js
@@ -1,31 +1,22 @@
 const DB = require('./dbHandlers.js')
-const base62 = require('./convertBase64.js')
+const crypto = require('crypto')
 module.exports = {
     //A. 輸入網址
     URL: function(req, res){
         //1. 取得長網址
         u_longURL = req.body.longURL
         //2. 確認輸入的網址有沒有資料了
-        DB.longURLExist(u_longURL)
+        DB.searchURLtbBylongURL(u_longURL)
         .then((result)=>{
-            if(result != null){
-                //3. 有存過了
-                DB.getShortURL(u_longURL)
+            if(result != null){//3. 有存過了
                 //4. 給一樣的
-                .then((result)=>{
-                    res.render('index', {shortURL: result.shortURL})
-                })
-            }
-            else{
-                //5. 沒有存過給新的
-                DB.getLastId()
-                .then((lastId)=>{
-                    //6. 算一個新的短網址
-                    lastId = lastId+=1
-                    let new_shortURL = base62(lastId)
-                    DB.createShortURL(new_shortURL, u_longURL)
-                    res.render('index', {shortURL: new_shortURL})
-                })
+                res.render('index', {shortURL: result.shortURL})
+            }else{
+                //5. 沒存過給新的
+                new_shortURL = crypto.randomBytes(3).toString('hex')
+                console.log(new_shortURL);
+                DB.createShortURL(new_shortURL, u_longURL)
+                res.render('index', {shortURL: new_shortURL})
             }
         })
     },
