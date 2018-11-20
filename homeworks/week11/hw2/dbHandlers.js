203f53a50c840a3cdbb6456c294c4ec6dc6e3c6d
yuchun33 <yuchunlin@gmail.com>
2018-11-20 19:45:41 +0800

    訂正第 11 週作業

--- homeworks/week11/hw2/dbHandlers.js
+++ homeworks/week11/hw2/dbHandlers.js
@@ -1,31 +1,23 @@
 const DB = require('./db.js')
 
 module.exports = {
-    //A. 有沒有存過了
-    longURLExist: function(u_longURL){
-        return DB.createURLtb().findOne({where: {longURL: u_longURL}})
+    //A. 用長網址找 URLtb
+    searchURLtbBylongURL: function(u_longURL){
+        return DB.URLtb.findOne({where: {longURL: u_longURL}})
     },
-    //B. 拿最新的 id 算新的短網址
-    getLastId: function(){
-        return DB.createURLtb().max('id')
-    },
-    //C. 新增短網址
+    //B. 新增短網址
     createShortURL: function(new_shortURL, u_longURL){
-        DB.createURLtb().create(
+        DB.URLtb.create(
             {longURL: u_longURL}
         ).then(()=>{
-            DB.createURLtb().update(
+            DB.URLtb.update(
                 {shortURL: new_shortURL},
                 {where: {longURL: u_longURL}}
             )
         })
     },
-    //D. 拿短網址
-    getShortURL: function(u_longURL){
-        return DB.createURLtb().findOne({where: {longURL: u_longURL}})
-    },
-    //E. 拿長網址
+    //C. 拿長網址
     getLongURL: function(u_shortURL){
-        return DB.createURLtb().findOne({where: {shortURL: u_shortURL}})
+        return DB.URLtb.findOne({where: {shortURL: u_shortURL}})
     }
 }
